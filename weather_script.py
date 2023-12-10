d10b3d3a84fe5615fbf1fff837b9db21267e0d79
luizhcsantos <lh.santos2008@gmail.com>
2023-12-10 20:26:11 -0300

    Script python corrigido - agora os dados da api podem ser inseridos no banco

--- weather_script.py
+++ weather_script.py
@@ -10,8 +10,8 @@ CITY = 'Presidente Prudente'
 COUNTRY = 'br'
 LAT = -22.1276
 LNG = -51.3856
-WEATHER_API_URL_ = f'http://api.openweathermap.org/data/2.5/weather?q={CITY},{COUNTRY}&appid={API_KEY}&units=metric'
-AP_API_URL_ = f'http://api.openweathermap.org/data/2.5/air_pollution?lat={LAT}&lon={LNG}&appid={API_KEY}'
+WEATHER_API_URL = f'http://api.openweathermap.org/data/2.5/weather?q={CITY},{COUNTRY}&appid={API_KEY}&units=metric'
+AP_API_URL = f'http://api.openweathermap.org/data/2.5/air_pollution?lat={LAT}&lon={LNG}&appid={API_KEY}'
 
 # Configurações do banco de dados MySQL
 DB_HOST = '127.0.0.1'
@@ -20,73 +20,55 @@ DB_PASSWORD = '273107'
 DB_NAME = 'openweather_db'
 
 
-def get_weather_data():
+def get_weather_data(api_url):
     try:
-        response_weather = requests.get(WEATHER_API_URL_)
-        response_weather.raise_for_status()
+        response = requests.get(api_url)
+        response.raise_for_status()
 
-        data_weather = response_weather.json()
-
-        response_ap = requests.get(AP_API_URL_)
-        response_ap.raise_for_status()
-
-        data_ap = response_ap.json()
-
-        # Inserir dados na tabela
-        try:
-            cidade = data_weather['name']
-            pais = data_weather['sys']['country']
-            temperatura = data_weather['main']['temp']
-            umidade = data_weather['main']['humidity']
-            pressao = data_weather['main']['pressure']
-            vel_vento = data_weather['wind']['speed']
-            dir_vento = data_weather['wind']['deg']
-            data_hora = datetime.fromtimestamp(data_weather['dt'], pytz.timezone('America/Sao_Paulo')).strftime(
-                '%Y-%m-%d %H:%M:%S')
-            descricao = data_weather['weather'][0]['description']
-
-            lat = data_ap['coord']['lat']
-            lng = data_ap['coord']['lon']
-            aqi = data_ap['main']['aqi']
-            co_conc = data_ap['components']['co']
-            no_conc = data_ap['components']['no']
-            no2_conc = data_ap['components']['no2']
-            o3_conc = data_ap['components']['o3']
-            so2_conc = data_ap['components']['so2']
-            pm2_5_conc = data_ap['components']['pm2_5']
-            pm10_conc = data_ap['components']['pm10']
-            nh3_conc = data_ap['components']['nh3']
-
-            """ if 'main' in data_weather and all(key in data_weather['main'] for key in ['temp', 'humidity', 'pressure']):
-                temperatura = data_weather['main']['temp']
-                umidade = data_weather['main']['humidity']
-                pressao = data_weather['main']['pressure']
-            else:
-                print("Alguma(s) chave(s) 'temp', 'humidity' ou 'pressure' não encontrada(s) nos dados da condição meteorológica.") """
-
-
-            print(f"Data e hora formatada: {data_hora}")
-
-            return cidade, pais, lat, lng, aqi, co_conc, no_conc, no2_conc, o3_conc, so2_conc, pm2_5_conc, pm10_conc, nh3_conc, temperatura, umidade, pressao, vel_vento, dir_vento, data_hora, descricao
-            
-        except KeyError as e:
-            print(f"Erro ao acessar chave: {e}")
-
-            if 'data_ap' in locals() and e.args and e.args[0] in data_ap:
-                print(f"A chave {e.args[0]} pertence à API de poluição do ar.")
-            elif 'data_weather' in locals() and e.args and e.args[0] in data_weather:
-                print(f"A chave {e.args[0]} pertence à API de condições meteorológicas.")
-            else:
-                print("Não foi possível determinar a origem do erro.")
-        
-        return 
+        return response.json()
 
     except Exception as e:
         print(f"Erro ao obter dados da API: {e}")
         return None
 
 
-def save_to_database(d):
+def extract_weather_data(weather_data):
+    cidade = weather_data['name']
+    pais = weather_data['sys']['country']
+    temperatura = weather_data['main']["temp"]
+    umidade = weather_data['main']['humidity']
+    pressao = weather_data['main']['pressure']
+    vel_vento = weather_data['wind']['speed']
+    dir_vento = weather_data['wind']['deg']
+    data_hora = datetime.utcfromtimestamp(weather_data['dt']).strftime('%Y-%m-%dT%H:%M:%SZ')
+    #data_hora = datetime.fromtimestamp(weather_data['dt'], pytz.timezone('America/Sao_Paulo')).strftime('%Y-%m-%d %H:%M:%S')
+    descricao = weather_data['weather'][0]['description']
+
+    return cidade, pais, temperatura, umidade, pressao, vel_vento, dir_vento, data_hora, descricao
+
+
+def extract_air_pollution_data(air_pollution_data):
+    lat = air_pollution_data['coord']['lat']
+    lng = air_pollution_data['coord']['lon']
+    temp = air_pollution_data['list'][0]
+    temp1 = temp['main']
+    # print(air_pollution_data['list'][0]['main']['aqi'])
+    aqi = temp1['aqi']
+    components = air_pollution_data['list'][0]['components']
+    print(components)
+    co_conc = components['co']
+    no_conc =  components['no']
+    no2_conc = components['no2']
+    o3_conc =  components['o3']
+    so2_conc =  components['so2']
+    pm2_5_conc =  components['pm2_5']
+    pm10_conc =  components['pm10']
+    nh3_conc =  components['nh3']
+
+    return lat, lng, aqi, co_conc, no_conc, no2_conc, o3_conc, so2_conc, pm2_5_conc, pm10_conc, nh3_conc
+
+
+def save_to_database(weather_data, air_pollution_data):
     try:
         # Conectar ao banco de dados MySQL
         connection = mysql.connector.connect(
@@ -97,17 +79,35 @@ def save_to_database(d):
         )
         cursor = connection.cursor()
 
-        query = """INSERT INTO weather (city, country, lat, lng, 
-        aqi, co_conc, no_conc, no2_conc, o3_conc, so2_conc, pm2_5_conc, pm10_conc, nh3_conc, 
-        temperature, humidity, pressure, 
-        wind_speed, wind_direction, updated_at, description) 
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+        # Extrair dados da API de condições meteorológicas
+        cidade, pais, temperatura, umidade, pressao, vel_vento, dir_vento, data_hora, descricao = extract_weather_data(
+            weather_data)
 
-        cursor.execute(query, d)
+        # Extrair dados da API de poluição do ar
+        lat, lng, aqi, co_conc, no_conc, no2_conc, o3_conc, so2_conc, pm2_5_conc, pm10_conc, nh3_conc = extract_air_pollution_data(
+            air_pollution_data)
+        
+
+        # Inserir dados na tabela
+        query = """INSERT INTO weather (`city`, `country`, `lat`, `lng`, 
+            `aqi`, `co_conc`, `no_conc`, `no2_conc`, `o3_conc`, `so2_conc`, `pm2_5_conc`, `pm10_conc`, `nh3_conc`, 
+            `temperature`, `humidity`, `pressure`, 
+            `wind_speed`, `wind_direction`, `description`) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+
+        data_to_insert = (cidade, pais, lat, lng, aqi, co_conc, no_conc, no2_conc, o3_conc, so2_conc, pm2_5_conc,
+                          pm10_conc, nh3_conc, temperatura, umidade, pressao, vel_vento, dir_vento, descricao)
+
+        cursor.execute(query, data_to_insert)
         connection.commit()
 
     except Exception as e:
         print(f"Erro ao salvar dados no banco de dados: {e}")
+
+    """ except mysql.connector.Error as e:
+        print(f"Erro ao salvar dados no banco de dados: {e}")
+        print(f"Estado da conexão: {connection}")
+        print(f"Dados a serem inseridos: {data_to_insert}") """
 
     # Fechar a conexão
     cursor.close()
@@ -117,10 +117,11 @@ def save_to_database(d):
 if __name__ == "__main__":
 
     while True:
-        weather_data = get_weather_data()
+        weather_data = get_weather_data(WEATHER_API_URL)
+        air_pollution_data = get_weather_data(AP_API_URL)
 
-        if weather_data:
-            save_to_database(weather_data)
+        if weather_data and air_pollution_data:
+            save_to_database(weather_data, air_pollution_data)
 
         print("Dados salvos no banco de dados.")
         sleep(1800)  # Espera 5 minutos antes de obter os dados novamente
